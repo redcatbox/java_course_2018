4ce1c75a04edbdaf11152531ee41009f1993653c
dbarannik <redcatbox@gmail.com>
2018-02-10 09:10:02 +0200

    WIP lab_2

--- src/main/java/home/dbarannik/labs/Lab_2_01_PasswordGenerator.java
+++ src/main/java/home/dbarannik/labs/Lab_2_01_PasswordGenerator.java
@@ -1,35 +1,65 @@
 package home.dbarannik.labs;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Lab_2_01_PasswordGenerator {
     static final int PASS_LENGTH_MIN = 8;
     static final int PASS_LENGTH_MAX = 20;
     static final String NUMBERS = "0123456789";
-    static final String Characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-    static final String SpecialCharacters = "!@#$%^&*";
+    static final String CHARACTERS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    static final String SPECIAL_CHARACTERS = "!@#$%^&*";
 
     public static void main(String[] args) {
         int passLength = getValidPassLength();
-        generatePassword(passLength);
+        System.out.println("Your password is: " + generatePass(passLength));
     }
 
     public static int getValidPassLength() {
-        System.out.println("Enter password length:");
-        Scanner console = new Scanner(System.in);
-        while(true) {
+        int result;
 
+        System.out.printf("Enter password length between %d and %d:%n", PASS_LENGTH_MIN, PASS_LENGTH_MAX);
+
+        Scanner console = new Scanner(System.in);
+        while (true) {
+            if (console.hasNextInt()) {
+                result = console.nextInt();
+                if (result >= PASS_LENGTH_MIN && result <= PASS_LENGTH_MAX) {
+                    break;
+                } else {
+                    System.out.println("Invalid input! Try again...");
+                }
+            } else {
+                System.out.println("Invalid input! Try again...");
+                console.next();
+            }
         }
-        return console.nextInt();
+        return result;
     }
 
-    public static String generatePassword(int length) {
-        String result;
-        while(true) {
-            break;
-        }
-        for(int i = 0; i < length; i++)
+    public static String generatePass(int length) {
+        String result = "";
+        boolean isHaveSpecialCharacter = false;
+        int numOfDigits = 0;
 
+        Random rnd = new Random();
+        numOfDigits = rnd.nextInt(3);
+
+        while (true) {
+            if (result.length() < length) {
+
+
+
+
+
+
+
+
+            } else {
+                break;
+            }
+
+        }
         return result;
     }
 }
