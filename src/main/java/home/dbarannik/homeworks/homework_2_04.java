6ea5310324eace707b7c147c09b6e6469384159c
dbarannik <redcatbox@gmail.com>
2018-02-10 09:47:37 +0200

    fixed code style

--- src/main/java/home/dbarannik/homeworks/homework_2_04.java
+++ src/main/java/home/dbarannik/homeworks/homework_2_04.java
@@ -5,40 +5,30 @@ import java.util.Scanner;
 public class homework_2_04 {
     public static void main(String[] args) {
         int number = getNumber();
-        if(validateIsNumberPowerOfTwo(number))
-        {
+        if (validateIsNumberPowerOfTwo(number)) {
             System.out.println("Number " + number + " \033[32;1;2mIS\033[0m power of two!");
-        }
-        else
-        {
+        } else {
             System.out.println("Number " + number + " \033[31;1mIS NOT\033[0m power of two!");
         }
     }
 
     public static int getNumber() {
-        Scanner console;
+        Scanner console = new Scanner(System.in);;
         int result;
         do {
             System.out.println("Enter number > 1:");
-            console = new Scanner(System.in);
             result = console.nextInt();
         }
-        while(result <= 1);
+        while (result <= 1);
         return result;
     }
 
     public static boolean validateIsNumberPowerOfTwo(int n) {
-
-        if((n % 2 == 0))
-        {
+        if (n % 2 == 0) {
             return validateIsNumberPowerOfTwo(n/2);
-        }
-        else if(n == 1)
-             {
+        } else if (n == 1) {
                  return true;
-             }
-             else
-             {
+             } else {
                  return false;
              }
     }
