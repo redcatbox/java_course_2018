af856d7cac8ef7be8ed474777e4eda703b932b4a
dbarannik <redcatbox@gmail.com>
2018-02-10 13:06:44 +0200

    finished homework_3_01

--- src/main/java/home/dbarannik/homeworks/homework_3_01.java
+++ src/main/java/home/dbarannik/homeworks/homework_3_01.java
@@ -10,22 +10,6 @@ public class homework_3_01 {
         System.out.println("Enter your string:"); //All those moments will be lost in time, like tears in rain.
         userString = getInputString();
 
-        // Show menu
-        showChoiceMenu();
-
-        // Get and process user choice
-        while(true)
-        {
-            getAndValidateUserChoice(userString);
-        }
-    }
-
-    public static String getInputString() {
-        Scanner console = new Scanner(System.in);
-        return console.nextLine();
-    }
-
-    public static void showChoiceMenu() {
         // Show choice menu
         System.out.println("Lets convert your string into something amazing!");
         System.out.println("Please, make your choice:");
@@ -35,6 +19,16 @@ public class homework_3_01 {
         System.out.println("D. eXAMPLE oF yOUR sTRING");
         System.out.println("E. Example of your string");
         System.out.println("F. Exit application");
+
+        // Get and process user choice
+        while (true) {
+            getAndValidateUserChoice(userString);
+        }
+    }
+
+    public static String getInputString() {
+        Scanner console = new Scanner(System.in);
+        return console.nextLine();
     }
 
     public static void getAndValidateUserChoice(String userString) {
@@ -43,8 +37,7 @@ public class homework_3_01 {
         final String errorMessage = "Error! Invalid choice. Please try again...";
         choiceString = getInputString();
 
-        if(choiceString.length() == 1)
-        {
+        if (choiceString.length() == 1) {
             // Get input char
             inputChar = choiceString.charAt(0);
 
@@ -78,42 +71,53 @@ public class homework_3_01 {
                     System.out.println(errorMessage);
                     break;
             }
-        }
-        else
-        {
+        } else {
             System.out.println(errorMessage);
         }
     }
 
-    public static String toUppercase(String inputString)
-    {
+    public static String toUppercase(String inputString) {
         return inputString.toUpperCase();
     }
 
-    public static String toLowercase(String inputString)
-    {
+    public static String toLowercase(String inputString) {
         return inputString.toLowerCase();
     }
 
-    public static String toCamelcase(String inputString)
-    {
-        String[] tmpStr;
-/*        for(inputString : )
-        {
-
-        }*/
-        return "Camelcase";
+    public static String toCamelcase(String inputString) {
+        String result = "";
+        String[] words = inputString.split(" ");
+        for (int i = 0; i < words.length ; i++) {
+            words[i] = replaceFirstChar(words[i], true);
+            result = result + words[i] + " ";
+        }
+        result = result.trim();
+        return result;
     }
 
-    public static String toInverseCamelcase(String inputString)
-    {
-        return "InverseCamelcase";
+    public static String toInverseCamelcase(String inputString) {
+        String result = "";
+        String[] words = inputString.split(" ");
+        for (int i = 0; i < words.length ; i++) {
+            words[i] = words[i].toUpperCase();
+            words[i] = replaceFirstChar(words[i], false);
+            result = result + words[i] + " ";
+        }
+        result = result.trim();
+        return result;
     }
 
-    public static String toSentencecase(String inputString)
-    {
+    public static String toSentencecase(String inputString) {
+        return replaceFirstChar(inputString, true);
+    }
+
+    public static String replaceFirstChar(String inputString, boolean isToUppercase) {
         char newFirstChar = inputString.charAt(0);
-        newFirstChar = Character.toUpperCase(newFirstChar);
+        if (isToUppercase) {
+            newFirstChar = Character.toUpperCase(newFirstChar);
+        } else {
+            newFirstChar = Character.toLowerCase(newFirstChar);
+        }
         StringBuilder newString = new StringBuilder(inputString);
         newString.setCharAt(0, newFirstChar);
         return newString.toString();
