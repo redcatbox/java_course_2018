96228ccbbe0accdee5b890afe4c8850ad953421b
dbarannik <redcatbox@gmail.com>
2018-02-11 13:21:54 +0200

    WIP HomeWork_3_02

--- src/main/java/home/dbarannik/homeworks/HomeWork_3_02.java
+++ src/main/java/home/dbarannik/homeworks/HomeWork_3_02.java
@@ -1,12 +1,13 @@
 package home.dbarannik.homeworks;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HomeWork_3_02 {
     public static void main(String[] args) {
-        String userString = getValidInputString();
+        String userString = getValidInputString(); //Mama papA totos kiwi
         calculateCharacters(userString);
     }
 
@@ -14,7 +15,7 @@ public class HomeWork_3_02 {
         String result = "";
         System.out.println("Enter string that contains only latin alphabet symbols:");
         Scanner console = new Scanner(System.in);
-        Pattern pat = Pattern.compile("[*a-zA-Z]+");
+        Pattern pat = Pattern.compile("[a-zA-Z\\s']+");
 
         while (true) {
             result = console.nextLine();
@@ -29,13 +30,28 @@ public class HomeWork_3_02 {
     }
 
     public static void calculateCharacters(String inputString) {
-        StringBuilder str = new StringBuilder(inputString);
-        char charCurrent;
+        int charCounter = 0;
+        String result;
+        char[] chars = inputString.toCharArray();
 
-        while (inputString.length() > 0) {
-            charCurrent = inputString.charAt(0);
+        // Get and sort user string
+        Arrays.sort(chars);
+        result = new String(chars);
+        //result = result.trim(); // Remove spaces
+        System.out.println("String = " + inputString);
+        System.out.println("Sorted = " + result);
+
+        for (int i = 0; i < chars.length; i++) {
+            if (i == 0) {
+                charCounter++;
+            } else {
+                if (chars[i] == chars[i - 1]) {
+                    charCounter++;
+                } else {
+                    System.out.printf("Character '%s' appears %d times.%n", chars[i - 1], charCounter);
+                    charCounter = 0;
+                }
+            }
         }
-
-
     }
 }
