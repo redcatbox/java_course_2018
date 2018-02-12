d71272997c46108b43cfbb782172c719e59edef8
dbarannik <redcatbox@gmail.com>
2018-02-12 10:30:43 +0200

    WIP HomeWork_3_03

--- src/main/java/home/dbarannik/homeworks/HomeWork_3_03.java
+++ src/main/java/home/dbarannik/homeworks/HomeWork_3_03.java
@@ -5,9 +5,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HomeWork_3_03 {
-    final static String REGEX_FULL_NAME = "";
-    final static String REGEX_PHONE_NUMBER = "";
-    final static String REGEX_EMAIL_ADDRESS = "";
+    final static String REGEX_FULL_NAME = "[a-zA-Z\\s']+";
+    final static String REGEX_PHONE_NUMBER = "[a-zA-Z\\s']+";
+    final static String REGEX_EMAIL_ADDRESS = "[a-zA-Z\\s']+";
     final static String ERROR_MESSAGE_FULL_NAME = "Invalid full name. Please try again...";
     final static String ERROR_MESSAGE_PHONE_NUMBER = "Invalid phone number. Please try again...";
     final static String ERROR_MESSAGE_EMAIL_ADDRESS = "Invalid email address. Please try again...";
@@ -21,17 +21,20 @@ public class HomeWork_3_03 {
         System.out.println("It can contain latin letters, hyphen and space.");
         System.out.println("Your full name is:");
         fullName = getAndValidateInputString(REGEX_FULL_NAME, ERROR_MESSAGE_FULL_NAME);
+        System.out.println("");
 
         System.out.println("Enter your phone number.");
         System.out.println("+380(XX)XXXXXXX");
         System.out.println("Your phone number is:");
         phoneNumber = getAndValidateInputString(REGEX_PHONE_NUMBER, ERROR_MESSAGE_PHONE_NUMBER);
+        System.out.println("");
 
         System.out.println("Enter your email address.");
         System.out.println("account@domain.com");
         System.out.println("It can contain latin letters, numbers, hyphen and underscore)");
         System.out.println("Your email address is:");
         emailAddress = getAndValidateInputString(REGEX_EMAIL_ADDRESS, ERROR_MESSAGE_EMAIL_ADDRESS);
+        System.out.println("");
 
         System.out.printf("Name:  %s", fullName);
         System.out.printf("Phone: %s", phoneNumber);
