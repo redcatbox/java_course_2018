71d0e1939e7f7033b4c6e60a3a04bbd58d5191c3
dbarannik <redcatbox@gmail.com>
2018-02-12 14:14:22 +0200

    finished HomeWork_3_03

--- src/main/java/home/dbarannik/homeworks/HomeWork_3_03.java
+++ src/main/java/home/dbarannik/homeworks/HomeWork_3_03.java
@@ -5,9 +5,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HomeWork_3_03 {
-    final static String REGEX_FULL_NAME = "[a-zA-Z\\s']+";
-    final static String REGEX_PHONE_NUMBER = "[a-zA-Z\\s']+";
-    final static String REGEX_EMAIL_ADDRESS = "[a-zA-Z\\s']+";
+    final static String REGEX_FULL_NAME = "[a-zA-Z\\s'\\-?]+";
+    final static String REGEX_PHONE_NUMBER = "^(\\+380\\()\\d{2}\\)\\d{7}$";
+    final static String REGEX_EMAIL_ADDRESS = "^[a-zA-Z0-9\\-?\\_?]+(\\@)[a-zA-Z]+(\\.)[a-zA-Z]+$";
     final static String ERROR_MESSAGE_FULL_NAME = "Invalid full name. Please try again...";
     final static String ERROR_MESSAGE_PHONE_NUMBER = "Invalid phone number. Please try again...";
     final static String ERROR_MESSAGE_EMAIL_ADDRESS = "Invalid email address. Please try again...";
@@ -18,27 +18,28 @@ public class HomeWork_3_03 {
         String emailAddress;
 
         System.out.println("Enter your full name.");
-        System.out.println("It can contain latin letters, hyphen and space.");
+        System.out.println("Last name + First name + Second name");
+        System.out.println("Can contain latin letters, hyphen and space.");
         System.out.println("Your full name is:");
-        fullName = getAndValidateInputString(REGEX_FULL_NAME, ERROR_MESSAGE_FULL_NAME);
+        fullName = getAndValidateInputString(REGEX_FULL_NAME, ERROR_MESSAGE_FULL_NAME); // Baran-nik Dmitriy Nikolaevich
         System.out.println("");
 
         System.out.println("Enter your phone number.");
         System.out.println("+380(XX)XXXXXXX");
         System.out.println("Your phone number is:");
-        phoneNumber = getAndValidateInputString(REGEX_PHONE_NUMBER, ERROR_MESSAGE_PHONE_NUMBER);
+        phoneNumber = getAndValidateInputString(REGEX_PHONE_NUMBER, ERROR_MESSAGE_PHONE_NUMBER); // +380(99)0234569
         System.out.println("");
 
         System.out.println("Enter your email address.");
         System.out.println("account@domain.com");
-        System.out.println("It can contain latin letters, numbers, hyphen and underscore)");
+        System.out.println("Can contain latin letters, numbers, hyphen and underscore)");
         System.out.println("Your email address is:");
-        emailAddress = getAndValidateInputString(REGEX_EMAIL_ADDRESS, ERROR_MESSAGE_EMAIL_ADDRESS);
+        emailAddress = getAndValidateInputString(REGEX_EMAIL_ADDRESS, ERROR_MESSAGE_EMAIL_ADDRESS); // Some_Account-01@mail.net
         System.out.println("");
 
-        System.out.printf("Name:  %s", fullName);
-        System.out.printf("Phone: %s", phoneNumber);
-        System.out.printf("Email: %s", emailAddress);
+        System.out.printf("Name:  %s.%n", fullName);
+        System.out.printf("Phone: %s.%n", phoneNumber);
+        System.out.printf("Email: %s.%n", emailAddress);
     }
 
     public static String getAndValidateInputString(String regex, String errorMessage) {
