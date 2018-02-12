2f61397e56144ceded417fe6a9fae78be48563f0
dbarannik <redcatbox@gmail.com>
2018-02-12 09:23:23 +0200

    WIP HomeWork_3_03

--- src/main/java/home/dbarannik/homeworks/HomeWork_3_03.java
+++ src/main/java/home/dbarannik/homeworks/HomeWork_3_03.java
@@ -2,18 +2,20 @@ package home.dbarannik.homeworks;
 
 public class HomeWork_3_03 {
     public static void main(String[] args) {
+        getInputFullName();
+        getInputPhoneNumber();
+        getInputEmailAdress();
+    }
+
+    public static String getInputFullName() {
 
     }
 
-    public static String fullName() {
+    public static String getInputPhoneNumber() {
 
     }
 
-    public static String fullName() {
-
-    }
-
-    public static String fullName() {
+    public static String getInputEmailAdress() {
 
     }
 }
