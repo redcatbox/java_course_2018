1a95ef46633270aefa6921c883684045d8494be5
dbarannik <redcatbox@gmail.com>
2018-03-09 11:36:50 +0200

    WIP CMD

--- src/main/java/home/dbarannik/homeworks/HomeWork_9/EmployeesMain.java
+++ src/main/java/home/dbarannik/homeworks/HomeWork_9/EmployeesMain.java
@@ -1,26 +1,51 @@
 package home.dbarannik.homeworks.HomeWork_9;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class EmployeesMain {
     public static void main(String[] args) {
-        generate();
+        generateEmployeesFile(100);
+
     }
 
-    static void generate() {
-        int employeesNumber = 100;
-        Random random = new Random();
-
+    static void generateEmployeesFile(int num) {
         final String[] surnames = {"Zayac", "Volk", "Barsuk", "Orel", "Belka", "Mysh", "Homyak", "Zmey", "Ryba", "Voron", "Soroka", "Koshka", "Gus", "Sinica", "Medved", "Suslik"};
         final String[] namesM = {"Nikolay", "Andrey", "Viktor", "Evgeniy", "Maksim", "Yuriy", "Dmitriy", "Igor", "Aleksandr", "Aleksey", "Lev", "Vladislav", "Vladimir", "Oleg", "Mikhail"};
-        final String[] namesF = {"Elizabeth", "Anna", "Maria", "Julia", "Svetlana", "Elena", "Tamara", "Yana", "Nina", "Natali", "Daria", "Viktoria", "Ekaterina", "Veronika", "Anastasia", "", "", "", "", "", "", "",};
+        final String[] namesF = {"Elizabeth", "Anna", "Maria", "Julia", "Svetlana", "Elena", "Tamara", "Yana", "Nina", "Natali", "Daria", "Viktoria", "Ekaterina", "Veronika", "Anastasia"};
         final String middleNameM = "Batkovich";
         final String middleNameF = "Batkovna";
 
-        for (int i = 0; i < employeesNumber; i++) {
+        // Create file
+        Path filePath = Paths.get("").toAbsolutePath();
+        filePath = filePath.resolve("EmployeesFile");
+        if (filePath.toFile().exists()) {
+            filePath.toFile().delete();
+        }
+
+        try {
+            Files.createFile(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Generate random employees
+        for (int i = 0; i < num; i++) {
+            Random random = new Random();
+
             // sex
-            Sex rndSex = Sex.FEMALE;
+            Sex rndSex = null;
             switch (random.nextInt(2)) {
                 case 0:
                     rndSex = Sex.MALE;
@@ -30,43 +55,53 @@ public class EmployeesMain {
                     break;
             }
 
-            //lastName
+            //lastName, firstName, middleName
             String rndLastName = surnames[random.nextInt(surnames.length)];
-
-            //firstName
             String rndFirstName;
-            if (rndSex == Sex.MALE) {
-                rndFirstName = namesM[random.nextInt(namesM.length)];
-            } else {
-                rndFirstName = namesF[random.nextInt(namesF.length)];
-            }
-
-            // middleName
             String rndMiddleName;
             if (rndSex == Sex.MALE) {
+                rndFirstName = namesM[random.nextInt(namesM.length)];
                 rndMiddleName = middleNameM;
             } else {
+                rndFirstName = namesF[random.nextInt(namesF.length)];
                 rndMiddleName = middleNameF;
             }
 
-            // birthDate
-            Date rndBirthDate;
+            // birthDate, startDate, endDate
+//            GregorianCalendar gc = new GregorianCalendar();
+//            int year = random.nextInt(50) + 1940;
+//            gc.set(gc.YEAR, year);
+//            int day = random.nextInt(gc.getActualMaximum(gc.DAY_OF_YEAR) + 1);
+//            gc.set(gc.DAY_OF_YEAR, day);
+
+
+            long  = -631152000000L + (Math.abs(random.nextLong()) % (40L * 31536000000L)); // 1950 - 1990
+            LocalDate rndBirthDate = new LocalDate(rndBirthDateMs);
+            LocalDateTime rndBirthDate = LocalDateTime.now();
+            Date  = new Date(rndBirthDateMs);
+
+            long rndStartDateMs = 946684800000L + (Math.abs(random.nextLong()) % (17L * 31536000000L)); // 2000 - 2017
+            Date rndStartDate = new Date(rndStartDateMs);
+
+            long rndEndDateMs = rndStartDateMs +;
+
+            Date rndEndDate = new Date(rndEndDateMs);
+            if (rndEndDate.after(LocalDateTime.now()))
+
             // INN
-            int rndINN = (int) (Math.random() * 2000000000);
-
-            // startDate
-            Date rndStartDate;
-
-            // endDate
-            Date rndEndDate;
+            int rndINN = random.nextInt(2000000000 - 1000000000) + 1000000000;
 
             // Salary
-            int rndSalary = (int) (Math.random() * 20000) + 5000;
+            int rndSalary = random.nextInt(25000 - 5000) + 5000;
 
-            System.out.println(rndLastName + "," + rndFirstName + "," + rndMiddleName + "," + rndINN + "," + rndSex + "," + rndSalary);
+            // Write to file
+            String str = rndLastName + "," + rndFirstName + "," + rndMiddleName + "," + rndBirthDate + "," + rndINN + "," + rndSex + "," + rndStartDate + "," + rndEndDate + "," + rndSalary + "\n";
+            try {
+                Files.write(filePath, str.getBytes(), StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
-
-
 }
 
