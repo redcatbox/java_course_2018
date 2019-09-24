b34bfc52ae5dddec1634c143ff90f659be7bdfdc
dbarannik <redcatbox@gmail.com>
2019-09-24 16:57:17 +0300

    added

---
+++ src/main/java/home/dbarannik/sergey/HW21/HW21.java
@@ -0,0 +1,42 @@
+package home.dbarannik.sergey.HW21;
+
+public class HW21 {
+ public static void main(String[] args) {
+
+
+
+ }
+}
+
+//Есть класс User:
+//
+//class User {
+// int id;
+// String name;
+// int age;
+//}
+//
+//Написать интерфейс Storage, и класс FileStorage который в конструкторе принимает имя файла где будут храниться данные:
+//
+//
+//- void removeAll()
+//
+//- void removeUser(int id)
+//
+//- void removeUserByName(String name)
+//
+//- void addUser(User user)
+//
+//- void updateUser(User user)
+//
+//- User getUser(int id)
+//
+//- List<User> getAllUsers()
+//
+//
+//
+//Продемонстрирвоать работу всех методов.
+//
+//Данные должны храниться в тектовом файле в формате JSON.
+//
+//При добавлении метод addUser должен назначить User уникальный id - порядковый номер.
---
+++ src/main/java/home/dbarannik/sergey/HW22/HW22.java
@@ -0,0 +1,39 @@
+package home.dbarannik.sergey.HW22;
+
+public class HW22 {
+ public static void main(String[] args) {
+
+
+
+ }
+}
+
+//Есть класс User:
+//
+//class User {
+// int id;
+// String name;
+// int age;
+//}
+//
+//id - autoincrement
+//Взять тот же интерфейс Storage что и в предыдущем задании, и написать реализацию с использованием базы данных(на выбор DJBC/Hibernate, H2/Postgress/Mysql):
+//
+//
+//- void removeAll()
+//
+//- void removeUser(int id)
+//
+//- void removeUserByName(String name)
+//
+//- void addUser(User user)
+//
+//- void updateUser(User user)
+//
+//- User getUser(int id)
+//
+//- List<User> getAllUsers()
+//
+//
+//
+//Продемонстрирвоать работу всех методов.
